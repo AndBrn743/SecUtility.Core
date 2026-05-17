81e2f895fc21705440df11959a705f8eb183b352
Andy Brown <55843047+andbrn743@users.noreply.github.com>
2026-05-17 23:01:43 +0800

    Define ConcatenatedTypeTuple

--- SecUtility/Collection/TypeTuple.hpp
+++ SecUtility/Collection/TypeTuple.hpp
@@ -10,4 +10,33 @@ namespace SecUtility
 
 	template <typename T, typename U>
 	using TypePair = TypeTuple<T, U>;
+
+
+	namespace Detail::TypeTuple
+	{
+		template <typename...>
+		struct concat;
+
+		template <typename T, typename U, typename... Vs>
+		struct concat<T, U, Vs...>
+		{
+			using type = concat<typename concat<T, U>::type, Vs...>;
+		};
+
+		template <typename... Ts>
+		struct concat<SecUtility::TypeTuple<Ts...>>
+		{
+			using type = SecUtility::TypeTuple<Ts...>;
+		};
+
+		template <typename... Ts, typename... Us>
+		struct concat<SecUtility::TypeTuple<Ts...>, SecUtility::TypeTuple<Us...>>
+		{
+			using type = SecUtility::TypeTuple<Ts..., Us...>;
+		};
+	}
+
+
+	template <typename... Ts>
+	using ConcatenatedTypeTuple = typename Detail::TypeTuple::concat<Ts...>::type;
 }
