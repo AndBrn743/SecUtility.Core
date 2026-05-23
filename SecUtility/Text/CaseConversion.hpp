4a644ff034350068ad2123951afe54ea1384d0bf
Andy Brown <55843047+andbrn743@users.noreply.github.com>
2026-05-23 15:56:20 +0800

    Export char case conversion

--- SecUtility/Text/CaseConversion.hpp
+++ SecUtility/Text/CaseConversion.hpp
@@ -9,10 +9,12 @@
 namespace SecUtility
 {
 	inline constexpr auto ToLower = [](const unsigned char c) noexcept { return std::tolower(c); };
+
 	inline constexpr auto ToUpper = [](const unsigned char c) noexcept { return std::toupper(c); };
 
 	inline constexpr auto AsciiToLower = [](const unsigned char c) noexcept
-	{ return (c >= 'A' && c <= 'Z') ? (c + ('a' - 'A')) : c; };
+	{ return c >= 'A' && c <= 'Z' ? c + ('a' - 'A') : c; };
+
 	inline constexpr auto AsciiToUpper = [](const unsigned char c) noexcept
-	{ return (c >= 'a' && c <= 'z') ? (c + ('A' - 'a')) : c; };
+	{ return c >= 'a' && c <= 'z' ? c + ('A' - 'a') : c; };
 }
