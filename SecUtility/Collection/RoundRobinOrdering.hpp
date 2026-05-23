b872078f4374c51a0cc5550b75e9d1dc625217b5
Andy Brown <55843047+andbrn743@users.noreply.github.com>
2026-05-23 23:00:15 +0800

    Export RoundRobinOrderingGenerator

---
+++ SecUtility/Collection/RoundRobinOrdering.hpp
@@ -0,0 +1,293 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025-2026 Andy Brown
+
+#pragma once
+
+#include <SecUtility/Collection/SubscriptBasedIterator.hpp>
+#include <utility>
+#include <vector>
+
+
+namespace SecUtility
+{
+	template <typename Index>
+	class RoundRobinOrderingGenerator
+	{
+	public:
+		class RoundRobinOrderingPairs : public std::vector<std::pair<Index, Index>>
+		{
+			using Base = std::vector<std::pair<Index, Index>>;
+
+		public:
+			using Base::Base;
+			using Base::operator=;
+
+			friend std::ostream& operator<<(std::ostream& os, const RoundRobinOrderingPairs& pairs)
+			{
+				os << '{';
+
+				if (!pairs.empty())
+				{
+					os << '(' << pairs[0].first << ", " << pairs[0].second << ')';
+
+					for (auto iterator = pairs.begin() + 1; iterator != pairs.end(); iterator++)
+					{
+						os << ", (" << iterator->first << ", " << iterator->second << ')';
+					}
+				}
+
+				return os << '}';
+			}
+		};
+
+		static RoundRobinOrderingPairs GenerateInitialPairs(const Index size,
+		                                                    Index initialValue = 0,
+		                                                    const Index invalidValue = static_cast<Index>(-1))
+		{
+			const auto n = (size + 1) / 2;
+			RoundRobinOrderingPairs result(n);
+
+			for (auto& pair : result)
+			{
+				pair = {initialValue, initialValue + n};
+				initialValue += 1;
+			}
+
+			if (size % 2 != 0)
+			{
+				result.back().second = invalidValue;
+			}
+
+			return result;
+		}
+
+		static RoundRobinOrderingPairs GenerateNextPairsFrom(const RoundRobinOrderingPairs& pairs)
+		{
+			const auto n = pairs.size();
+			RoundRobinOrderingPairs result(n);
+
+#if defined(_OPENMP)
+#pragma omp parallel for default(none) shared(n, pairs, result) schedule(static)
+#endif
+			for (std::size_t k = 0; k < n; k++)
+			{
+				if (k == 0)
+				{
+					result[k].first = pairs[k].first;
+				}
+				else if (k == 1)
+				{
+					result[k].first = pairs[k - 1].second;
+				}
+				else
+				{
+					result[k].first = pairs[k - 1].first;
+				}
+
+				if (k == n - 1)
+				{
+					result[k].second = pairs[k].first;
+				}
+				else
+				{
+					result[k].second = pairs[k + 1].second;
+				}
+			}
+
+			return result;
+		}
+
+		static void UpdatePairs(RoundRobinOrderingPairs& ref_pairs)
+		{
+			ref_pairs = GenerateNextPairsFrom(ref_pairs);
+		}
+	};
+
+
+	template <typename Derived>
+	class AbstractRoundRobinOrdering
+	{
+		friend Derived;
+
+	public:
+		/* CRTP VIRTUAL */ void NextCycle() noexcept
+		{
+			AsDerived().NextCycle();
+		}
+
+		/* CRTP VIRTUAL */ auto PairsPerCycle() const noexcept
+		{
+			return AsDerived().PairsPerCycle();
+		}
+
+		/* CRTP VIRTUAL */ auto Period() const noexcept
+		{
+			return AsDerived().Period();
+		}
+
+		decltype(auto) operator[](const std::int64_t index) const noexcept(noexcept(AsDerived()[index]))
+		{
+			return AsDerived()[index];
+		}
+
+		using Iterator = SubscriptBasedIterator<Derived, std::int64_t>;
+
+		Iterator begin()
+		{
+			return {AsDerived(), 0};
+		}
+
+		Iterator end()
+		{
+			return {AsDerived(), PairsPerCycle()};
+		}
+
+
+	private:
+		constexpr AbstractRoundRobinOrdering() = default;
+		constexpr AbstractRoundRobinOrdering(const AbstractRoundRobinOrdering&) noexcept = default;
+		constexpr AbstractRoundRobinOrdering(AbstractRoundRobinOrdering&&) noexcept = default;
+		constexpr AbstractRoundRobinOrdering& operator=(const AbstractRoundRobinOrdering&) noexcept = default;
+		constexpr AbstractRoundRobinOrdering& operator=(AbstractRoundRobinOrdering&&) noexcept = default;
+		~AbstractRoundRobinOrdering() noexcept = default;
+
+		Derived& AsDerived() noexcept
+		{
+			return *static_cast<Derived*>(this);
+		}
+
+		const Derived& AsDerived() const noexcept
+		{
+			return *static_cast<const Derived*>(this);
+		}
+	};
+
+
+	template <typename Range>
+	class RoundRobinOrdering : public AbstractRoundRobinOrdering<RoundRobinOrdering<Range>>
+	{
+	private:
+		using Base = AbstractRoundRobinOrdering<RoundRobinOrdering<Range>>;
+		using Item = std::decay_t<decltype(*std::declval<Range>().begin())>;
+		Range m_Range;
+		Item m_InvalidItemMarker;
+		std::int64_t m_RangeSize;
+		std::int64_t m_EffectiveAndActiveRangeSize;
+		std::int64_t m_PairsPerCycle;
+		std::int64_t m_Period;
+		std::int64_t m_RobinIndex;
+
+	public:
+		RoundRobinOrdering(Range range, const Item invalidItemMarker)
+		    : m_Range(std::move(range)), m_InvalidItemMarker(invalidItemMarker),
+		      m_RangeSize(std::distance(std::begin(m_Range), std::end(m_Range))),
+		      m_EffectiveAndActiveRangeSize(m_RangeSize % 2 == 0 ? m_RangeSize - 1 : m_RangeSize),
+		      m_PairsPerCycle(m_RangeSize % 2 == 0 ? m_RangeSize / 2 : m_RangeSize / 2 + 1),
+		      m_Period(m_RangeSize * (m_RangeSize - 1) / 2 / (m_RangeSize / 2)), m_RobinIndex(0)
+		{
+			/* NO CODE */
+		}
+
+		/* CRTP OVERRIDE */ void NextCycle() noexcept
+		{
+			m_RobinIndex = (m_RobinIndex + 1) % Period();
+		}
+
+		/* CRTP OVERRIDE */ std::int64_t PairsPerCycle() const noexcept
+		{
+			return m_PairsPerCycle;
+		}
+
+		/* CRTP OVERRIDE */ std::int64_t Period() const noexcept
+		{
+			return m_Period;
+		}
+
+		/* CRTP OVERRIDE */ std::pair<Item, Item> operator[](const std::int64_t index) const
+		{
+			const auto i = index == 0 ? 0
+			                          : (index > m_RobinIndex ? index - m_RobinIndex
+			                                                  : m_EffectiveAndActiveRangeSize + index - m_RobinIndex);
+
+			const auto j = [this, index]
+			{
+				if (m_EffectiveAndActiveRangeSize <= m_RobinIndex + index)
+				{
+					return 2 * m_EffectiveAndActiveRangeSize - (m_RobinIndex + index);
+				}
+
+				return m_EffectiveAndActiveRangeSize - (m_RobinIndex + index);
+			}();
+
+			return {std::move(ItemAt(i)), std::move(ItemAt(j))};
+		}
+
+
+	private:
+		auto ItemAt(const std::int64_t index) const
+		{
+			if (index < m_RangeSize)
+			{
+				return m_Range[index];
+			}
+			else
+			{
+				return m_InvalidItemMarker;
+			}
+		}
+	};
+
+
+	template <typename Range>
+	class BiPartiteRoundRobinOrdering : public AbstractRoundRobinOrdering<BiPartiteRoundRobinOrdering<Range>>
+	{
+	private:
+		using Base = AbstractRoundRobinOrdering<BiPartiteRoundRobinOrdering<Range>>;
+		using Item = std::decay_t<decltype(*std::declval<Range>().begin())>;
+		Range m_Range0;
+		Range m_Range1;
+		std::int64_t m_RangeSize0;
+		std::int64_t m_RangeSize1;
+		std::int64_t m_PairsPerCycle;
+		std::int64_t m_Period;
+		std::int64_t m_RobinIndex;
+
+	public:
+		BiPartiteRoundRobinOrdering(Range range0, Range range1)
+		    : m_Range0(std::move(range0)), m_Range1(std::move(range1)),
+		      m_RangeSize0(std::distance(std::begin(m_Range0), std::end(m_Range0))),
+		      m_RangeSize1(std::distance(std::begin(m_Range1), std::end(m_Range1))),
+		      m_PairsPerCycle(std::min(m_RangeSize0, m_RangeSize1)), m_Period(std::max(m_RangeSize0, m_RangeSize1)),
+		      m_RobinIndex(0)
+		{
+			/* NO CODE */
+		}
+
+		/* CRTP OVERRIDE */ void NextCycle() noexcept
+		{
+			m_RobinIndex = (m_RobinIndex + 1) % Period();
+		}
+
+		/* CRTP OVERRIDE */ std::int64_t PairsPerCycle() const noexcept
+		{
+			return m_PairsPerCycle;
+		}
+
+		/* CRTP OVERRIDE */ std::int64_t Period() const noexcept
+		{
+			return m_Period;
+		}
+
+		/* CRTP OVERRIDE */ std::pair<Item, Item> operator[](const std::int64_t index) const
+		{
+			if (m_RangeSize0 <= m_RangeSize1)
+			{
+				return {m_Range0[index], m_Range1[(index + m_RobinIndex) % m_RangeSize1]};
+			}
+			else
+			{
+				return {m_Range0[(index + m_RobinIndex) % m_RangeSize0], m_Range1[index]};
+			}
+		}
+	};
+}
---
+++ tests/RoundRobinOrderingGeneratorTest.cpp
@@ -0,0 +1,585 @@
+//
+// Created by Andy on 10/21/2025.
+//
+
+#include <iostream>
+#include <set>
+#include <utility>
+
+std::ostream& operator<<(std::ostream& os, std::pair<int, int> p)
+{
+	return os << '{' << p.first << ", " << p.second << '}';
+}
+
+#include <SecUtility/Collection/RoundRobinOrdering.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers.hpp>
+
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-literal-operator"
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#endif
+#include <range/v3/view/iota.hpp>
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#endif
+
+TEST_CASE("Even")
+{
+	auto pairs = SecUtility::RoundRobinOrderingGenerator<int>::GenerateInitialPairs(6);
+	CHECK(pairs.size() == 3);
+	CHECK(pairs == std::vector<std::pair<int, int>>{{0, 3}, {1, 4}, {2, 5}});
+	SecUtility::RoundRobinOrderingGenerator<int>::UpdatePairs(pairs);
+	CHECK(pairs == std::vector<std::pair<int, int>>{{0, 4}, {3, 5}, {1, 2}});
+	SecUtility::RoundRobinOrderingGenerator<int>::UpdatePairs(pairs);
+	CHECK(pairs == std::vector<std::pair<int, int>>{{0, 5}, {4, 2}, {3, 1}});
+	SecUtility::RoundRobinOrderingGenerator<int>::UpdatePairs(pairs);
+	CHECK(pairs == std::vector<std::pair<int, int>>{{0, 2}, {5, 1}, {4, 3}});
+	SecUtility::RoundRobinOrderingGenerator<int>::UpdatePairs(pairs);
+	CHECK(pairs == std::vector<std::pair<int, int>>{{0, 1}, {2, 3}, {5, 4}});
+	SecUtility::RoundRobinOrderingGenerator<int>::UpdatePairs(pairs);
+	CHECK(pairs == std::vector<std::pair<int, int>>{{0, 3}, {1, 4}, {2, 5}});
+}
+
+TEST_CASE("Even2")
+{
+	auto pairs = SecUtility::RoundRobinOrdering{std::array{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, -1};
+
+	for (int i = 1; i < 32; i++)
+	{
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 9});
+		CHECK(pairs[1] == std::pair{1, 8});
+		CHECK(pairs[2] == std::pair{2, 7});
+		CHECK(pairs[3] == std::pair{3, 6});
+		CHECK(pairs[4] == std::pair{4, 5});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 8});
+		CHECK(pairs[1] == std::pair{9, 7});
+		CHECK(pairs[2] == std::pair{1, 6});
+		CHECK(pairs[3] == std::pair{2, 5});
+		CHECK(pairs[4] == std::pair{3, 4});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 7});
+		CHECK(pairs[1] == std::pair{8, 6});
+		CHECK(pairs[2] == std::pair{9, 5});
+		CHECK(pairs[3] == std::pair{1, 4});
+		CHECK(pairs[4] == std::pair{2, 3});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 6});
+		CHECK(pairs[1] == std::pair{7, 5});
+		CHECK(pairs[2] == std::pair{8, 4});
+		CHECK(pairs[3] == std::pair{9, 3});
+		CHECK(pairs[4] == std::pair{1, 2});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 5});
+		CHECK(pairs[1] == std::pair{6, 4});
+		CHECK(pairs[2] == std::pair{7, 3});
+		CHECK(pairs[3] == std::pair{8, 2});
+		CHECK(pairs[4] == std::pair{9, 1});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 4});
+		CHECK(pairs[1] == std::pair{5, 3});
+		CHECK(pairs[2] == std::pair{6, 2});
+		CHECK(pairs[3] == std::pair{7, 1});
+		CHECK(pairs[4] == std::pair{8, 9});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 3});
+		CHECK(pairs[1] == std::pair{4, 2});
+		CHECK(pairs[2] == std::pair{5, 1});
+		CHECK(pairs[3] == std::pair{6, 9});
+		CHECK(pairs[4] == std::pair{7, 8});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 2});
+		CHECK(pairs[1] == std::pair{3, 1});
+		CHECK(pairs[2] == std::pair{4, 9});
+		CHECK(pairs[3] == std::pair{5, 8});
+		CHECK(pairs[4] == std::pair{6, 7});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 1});
+		CHECK(pairs[1] == std::pair{2, 9});
+		CHECK(pairs[2] == std::pair{3, 8});
+		CHECK(pairs[3] == std::pair{4, 7});
+		CHECK(pairs[4] == std::pair{5, 6});
+
+		//--------------------------------------------------------------------------------------------------------------
+
+		pairs.NextCycle();
+	}
+}
+
+TEST_CASE("Even3")
+{
+	auto pairs = SecUtility::RoundRobinOrdering{ranges::views::iota(0, 10), -1};
+
+	for (int i = 1; i < 32; i++)
+	{
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 9});
+		CHECK(pairs[1] == std::pair{1, 8});
+		CHECK(pairs[2] == std::pair{2, 7});
+		CHECK(pairs[3] == std::pair{3, 6});
+		CHECK(pairs[4] == std::pair{4, 5});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 8});
+		CHECK(pairs[1] == std::pair{9, 7});
+		CHECK(pairs[2] == std::pair{1, 6});
+		CHECK(pairs[3] == std::pair{2, 5});
+		CHECK(pairs[4] == std::pair{3, 4});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 7});
+		CHECK(pairs[1] == std::pair{8, 6});
+		CHECK(pairs[2] == std::pair{9, 5});
+		CHECK(pairs[3] == std::pair{1, 4});
+		CHECK(pairs[4] == std::pair{2, 3});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 6});
+		CHECK(pairs[1] == std::pair{7, 5});
+		CHECK(pairs[2] == std::pair{8, 4});
+		CHECK(pairs[3] == std::pair{9, 3});
+		CHECK(pairs[4] == std::pair{1, 2});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 5});
+		CHECK(pairs[1] == std::pair{6, 4});
+		CHECK(pairs[2] == std::pair{7, 3});
+		CHECK(pairs[3] == std::pair{8, 2});
+		CHECK(pairs[4] == std::pair{9, 1});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 4});
+		CHECK(pairs[1] == std::pair{5, 3});
+		CHECK(pairs[2] == std::pair{6, 2});
+		CHECK(pairs[3] == std::pair{7, 1});
+		CHECK(pairs[4] == std::pair{8, 9});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 3});
+		CHECK(pairs[1] == std::pair{4, 2});
+		CHECK(pairs[2] == std::pair{5, 1});
+		CHECK(pairs[3] == std::pair{6, 9});
+		CHECK(pairs[4] == std::pair{7, 8});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 2});
+		CHECK(pairs[1] == std::pair{3, 1});
+		CHECK(pairs[2] == std::pair{4, 9});
+		CHECK(pairs[3] == std::pair{5, 8});
+		CHECK(pairs[4] == std::pair{6, 7});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 1});
+		CHECK(pairs[1] == std::pair{2, 9});
+		CHECK(pairs[2] == std::pair{3, 8});
+		CHECK(pairs[3] == std::pair{4, 7});
+		CHECK(pairs[4] == std::pair{5, 6});
+
+		//--------------------------------------------------------------------------------------------------------------
+
+		pairs.NextCycle();
+	}
+}
+
+TEST_CASE("Odd")
+{
+	auto pairs = SecUtility::RoundRobinOrderingGenerator<int>::GenerateInitialPairs(5, 0, 9);
+	CHECK(pairs.size() == 3);
+	CHECK(pairs == std::vector<std::pair<int, int>>{{0, 3}, {1, 4}, {2, 9}});
+	SecUtility::RoundRobinOrderingGenerator<int>::UpdatePairs(pairs);
+	CHECK(pairs == std::vector<std::pair<int, int>>{{0, 4}, {3, 9}, {1, 2}});
+	SecUtility::RoundRobinOrderingGenerator<int>::UpdatePairs(pairs);
+	CHECK(pairs == std::vector<std::pair<int, int>>{{0, 9}, {4, 2}, {3, 1}});
+	SecUtility::RoundRobinOrderingGenerator<int>::UpdatePairs(pairs);
+	CHECK(pairs == std::vector<std::pair<int, int>>{{0, 2}, {9, 1}, {4, 3}});
+	SecUtility::RoundRobinOrderingGenerator<int>::UpdatePairs(pairs);
+	CHECK(pairs == std::vector<std::pair<int, int>>{{0, 1}, {2, 3}, {9, 4}});
+	SecUtility::RoundRobinOrderingGenerator<int>::UpdatePairs(pairs);
+	CHECK(pairs == std::vector<std::pair<int, int>>{{0, 3}, {1, 4}, {2, 9}});
+}
+
+
+TEST_CASE("Odd2")
+{
+	auto pairs = SecUtility::RoundRobinOrdering{std::array{0, 1, 2, 3, 4, 5, 6, 7, 8}, 9};
+
+	for (int i = 1; i < 32; i++)
+	{
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 9});
+		CHECK(pairs[1] == std::pair{1, 8});
+		CHECK(pairs[2] == std::pair{2, 7});
+		CHECK(pairs[3] == std::pair{3, 6});
+		CHECK(pairs[4] == std::pair{4, 5});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 8});
+		CHECK(pairs[1] == std::pair{9, 7});
+		CHECK(pairs[2] == std::pair{1, 6});
+		CHECK(pairs[3] == std::pair{2, 5});
+		CHECK(pairs[4] == std::pair{3, 4});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 7});
+		CHECK(pairs[1] == std::pair{8, 6});
+		CHECK(pairs[2] == std::pair{9, 5});
+		CHECK(pairs[3] == std::pair{1, 4});
+		CHECK(pairs[4] == std::pair{2, 3});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 6});
+		CHECK(pairs[1] == std::pair{7, 5});
+		CHECK(pairs[2] == std::pair{8, 4});
+		CHECK(pairs[3] == std::pair{9, 3});
+		CHECK(pairs[4] == std::pair{1, 2});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 5});
+		CHECK(pairs[1] == std::pair{6, 4});
+		CHECK(pairs[2] == std::pair{7, 3});
+		CHECK(pairs[3] == std::pair{8, 2});
+		CHECK(pairs[4] == std::pair{9, 1});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 4});
+		CHECK(pairs[1] == std::pair{5, 3});
+		CHECK(pairs[2] == std::pair{6, 2});
+		CHECK(pairs[3] == std::pair{7, 1});
+		CHECK(pairs[4] == std::pair{8, 9});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 3});
+		CHECK(pairs[1] == std::pair{4, 2});
+		CHECK(pairs[2] == std::pair{5, 1});
+		CHECK(pairs[3] == std::pair{6, 9});
+		CHECK(pairs[4] == std::pair{7, 8});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 2});
+		CHECK(pairs[1] == std::pair{3, 1});
+		CHECK(pairs[2] == std::pair{4, 9});
+		CHECK(pairs[3] == std::pair{5, 8});
+		CHECK(pairs[4] == std::pair{6, 7});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 1});
+		CHECK(pairs[1] == std::pair{2, 9});
+		CHECK(pairs[2] == std::pair{3, 8});
+		CHECK(pairs[3] == std::pair{4, 7});
+		CHECK(pairs[4] == std::pair{5, 6});
+
+		//--------------------------------------------------------------------------------------------------------------
+
+		pairs.NextCycle();
+	}
+}
+
+
+TEST_CASE("Odd3")
+{
+	auto pairs = SecUtility::RoundRobinOrdering{std::array{0, 1, 2, 3, 4, 5, 6, 7, 8}, -1};
+
+	for (int i = 1; i < 32; i++)
+	{
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, -1});
+		CHECK(pairs[1] == std::pair{1, 8});
+		CHECK(pairs[2] == std::pair{2, 7});
+		CHECK(pairs[3] == std::pair{3, 6});
+		CHECK(pairs[4] == std::pair{4, 5});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 8});
+		CHECK(pairs[1] == std::pair{-1, 7});
+		CHECK(pairs[2] == std::pair{1, 6});
+		CHECK(pairs[3] == std::pair{2, 5});
+		CHECK(pairs[4] == std::pair{3, 4});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 7});
+		CHECK(pairs[1] == std::pair{8, 6});
+		CHECK(pairs[2] == std::pair{-1, 5});
+		CHECK(pairs[3] == std::pair{1, 4});
+		CHECK(pairs[4] == std::pair{2, 3});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 6});
+		CHECK(pairs[1] == std::pair{7, 5});
+		CHECK(pairs[2] == std::pair{8, 4});
+		CHECK(pairs[3] == std::pair{-1, 3});
+		CHECK(pairs[4] == std::pair{1, 2});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 5});
+		CHECK(pairs[1] == std::pair{6, 4});
+		CHECK(pairs[2] == std::pair{7, 3});
+		CHECK(pairs[3] == std::pair{8, 2});
+		CHECK(pairs[4] == std::pair{-1, 1});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 4});
+		CHECK(pairs[1] == std::pair{5, 3});
+		CHECK(pairs[2] == std::pair{6, 2});
+		CHECK(pairs[3] == std::pair{7, 1});
+		CHECK(pairs[4] == std::pair{8, -1});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 3});
+		CHECK(pairs[1] == std::pair{4, 2});
+		CHECK(pairs[2] == std::pair{5, 1});
+		CHECK(pairs[3] == std::pair{6, -1});
+		CHECK(pairs[4] == std::pair{7, 8});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 2});
+		CHECK(pairs[1] == std::pair{3, 1});
+		CHECK(pairs[2] == std::pair{4, -1});
+		CHECK(pairs[3] == std::pair{5, 8});
+		CHECK(pairs[4] == std::pair{6, 7});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 1});
+		CHECK(pairs[1] == std::pair{2, -1});
+		CHECK(pairs[2] == std::pair{3, 8});
+		CHECK(pairs[3] == std::pair{4, 7});
+		CHECK(pairs[4] == std::pair{5, 6});
+
+		//--------------------------------------------------------------------------------------------------------------
+
+		pairs.NextCycle();
+	}
+}
+
+
+TEST_CASE("Odd4")
+{
+	auto pairs = SecUtility::RoundRobinOrdering{ranges::views::iota(0, 9), -1};
+
+	for (int i = 1; i < 32; i++)
+	{
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, -1});
+		CHECK(pairs[1] == std::pair{1, 8});
+		CHECK(pairs[2] == std::pair{2, 7});
+		CHECK(pairs[3] == std::pair{3, 6});
+		CHECK(pairs[4] == std::pair{4, 5});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 8});
+		CHECK(pairs[1] == std::pair{-1, 7});
+		CHECK(pairs[2] == std::pair{1, 6});
+		CHECK(pairs[3] == std::pair{2, 5});
+		CHECK(pairs[4] == std::pair{3, 4});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 7});
+		CHECK(pairs[1] == std::pair{8, 6});
+		CHECK(pairs[2] == std::pair{-1, 5});
+		CHECK(pairs[3] == std::pair{1, 4});
+		CHECK(pairs[4] == std::pair{2, 3});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 6});
+		CHECK(pairs[1] == std::pair{7, 5});
+		CHECK(pairs[2] == std::pair{8, 4});
+		CHECK(pairs[3] == std::pair{-1, 3});
+		CHECK(pairs[4] == std::pair{1, 2});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 5});
+		CHECK(pairs[1] == std::pair{6, 4});
+		CHECK(pairs[2] == std::pair{7, 3});
+		CHECK(pairs[3] == std::pair{8, 2});
+		CHECK(pairs[4] == std::pair{-1, 1});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 4});
+		CHECK(pairs[1] == std::pair{5, 3});
+		CHECK(pairs[2] == std::pair{6, 2});
+		CHECK(pairs[3] == std::pair{7, 1});
+		CHECK(pairs[4] == std::pair{8, -1});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 3});
+		CHECK(pairs[1] == std::pair{4, 2});
+		CHECK(pairs[2] == std::pair{5, 1});
+		CHECK(pairs[3] == std::pair{6, -1});
+		CHECK(pairs[4] == std::pair{7, 8});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 2});
+		CHECK(pairs[1] == std::pair{3, 1});
+		CHECK(pairs[2] == std::pair{4, -1});
+		CHECK(pairs[3] == std::pair{5, 8});
+		CHECK(pairs[4] == std::pair{6, 7});
+
+		pairs.NextCycle();
+		CHECK(pairs.PairsPerCycle() == 5);
+		CHECK(pairs[0] == std::pair{0, 1});
+		CHECK(pairs[1] == std::pair{2, -1});
+		CHECK(pairs[2] == std::pair{3, 8});
+		CHECK(pairs[3] == std::pair{4, 7});
+		CHECK(pairs[4] == std::pair{5, 6});
+
+		//--------------------------------------------------------------------------------------------------------------
+
+		pairs.NextCycle();
+	}
+}
+
+TEST_CASE("BiPartiteRoundRobinOrdering basic functionality")
+{
+	std::vector<int> a{0, 1, 2};
+	std::vector<int> b{10, 11, 12};
+	SecUtility::BiPartiteRoundRobinOrdering rr(a, b);
+
+	SECTION("Sizes are correct")
+	{
+		REQUIRE(rr.PairsPerCycle() == 3);
+		REQUIRE(rr.Period() == 3);
+	}
+
+	SECTION("Round 0 yields correct pairs")
+	{
+		auto p0 = rr[0];
+		REQUIRE(p0 == std::pair{0, 10});
+		auto p1 = rr[1];
+		REQUIRE(p1 == std::pair{1, 11});
+		auto p2 = rr[2];
+		REQUIRE(p2 == std::pair{2, 12});
+	}
+
+	SECTION("Rotation works correctly")
+	{
+		rr.NextCycle();  // move to round 1
+		REQUIRE(rr[0] == std::pair{0, 11});
+		REQUIRE(rr[1] == std::pair{1, 12});
+		REQUIRE(rr[2] == std::pair{2, 10});
+	}
+
+	SECTION("Multiple full rotations return to start")
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			rr.NextCycle();
+		}
+		REQUIRE(rr[0] == std::pair{0, 10});
+	}
+}
+
+TEST_CASE("BiPartiteRoundRobinOrdering asymmetric sizes")
+{
+	std::vector<int> a{0, 1, 2};
+	std::vector<int> b{10, 11, 12, 13};
+	SecUtility::BiPartiteRoundRobinOrdering rr(a, b);
+
+	SECTION("Pairs per round is min(a,b)")
+	{
+		REQUIRE(rr.PairsPerCycle() == 3);
+	}
+
+	SECTION("Period is max(a,b)")
+	{
+		REQUIRE(rr.Period() == 4);
+	}
+
+	SECTION("All pairs eventually appear")
+	{
+		std::set<std::pair<int, int>> seen;
+		for (int round = 0; round < rr.Period(); ++round)
+		{
+			for (int i = 0; i < rr.PairsPerCycle(); ++i)
+			{
+				seen.insert(rr[i]);
+			}
+			rr.NextCycle();
+		}
+
+		// All 12 pairs should appear exactly once
+		std::set<std::pair<int, int>> expected{{0, 10},
+		                                       {0, 11},
+		                                       {0, 12},
+		                                       {0, 13},
+		                                       {1, 10},
+		                                       {1, 11},
+		                                       {1, 12},
+		                                       {1, 13},
+		                                       {2, 10},
+		                                       {2, 11},
+		                                       {2, 12},
+		                                       {2, 13}};
+		REQUIRE(seen == expected);
+	}
+}
+
+TEST_CASE("BiPartiteRoundRobinOrdering multiple passes with rotation")
+{
+	std::vector<int> a{0, 1};
+	std::vector<int> b{100, 101, 102};
+	SecUtility::BiPartiteRoundRobinOrdering rr(a, b);
+
+	SECTION("Rotation offset accumulates correctly")
+	{
+		rr.NextCycle();  // round 1
+		REQUIRE(rr[0] == std::pair{0, 101});
+		REQUIRE(rr[1] == std::pair{1, 102});
+
+		rr.NextCycle();  // round 2
+		REQUIRE(rr[0] == std::pair{0, 102});
+		REQUIRE(rr[1] == std::pair{1, 100});
+
+		rr.NextCycle();  // round 3
+		REQUIRE(rr[0] == std::pair{0, 100});
+		REQUIRE(rr[1] == std::pair{1, 101});
+	}
+}
