bdfda9429bb1d6a4b92766b0638fe935c3fbe795
Andy Brown <55843047+andbrn743@users.noreply.github.com>
2026-06-26 20:47:33 +0800

    tweak TypeNameTest.cpp for MSVC

--- tests/TypeNameTest.cpp
+++ tests/TypeNameTest.cpp
@@ -29,10 +29,12 @@ TEST_CASE("Constexpr TypeName should work with builtins")
 
 	REQUIRE(Matched(SecUtility::TypeName<const int&&>, std::regex{R"(^const int\s*&&$)"}));
 
+	// what is MSVC doing?
 	REQUIRE((SecUtility::TypeName<long const volatile long> == "volatile const long long"
 	         || SecUtility::TypeName<long const volatile long> == "const volatile long long"
 	         || SecUtility::TypeName<long const volatile long> == "volatile const long long int"
-	         || SecUtility::TypeName<long const volatile long> == "const volatile long long int"));
+	         || SecUtility::TypeName<long const volatile long> == "const volatile long long int"
+	         || SecUtility::TypeName<long const volatile long> == "volatile const __int64"));
 }
 
 namespace Example::Internal
@@ -46,29 +48,42 @@ namespace Example::Internal
 
 TEST_CASE("Constexpr TypeName should work with user defined")
 {
+#if defined(_MSC_VER)
+	STATIC_REQUIRE(SecUtility::TypeName<Example::Internal::Array<int, 3>>
+	               == std::string_view{"class Example::Internal::Array<int,3>"});
+
+	STATIC_REQUIRE(SecUtility::TypeName<const Example::Internal::Array<int, 4>>
+	               == std::string_view{"const class Example::Internal::Array<int,4>"});
+#else
 	STATIC_REQUIRE(SecUtility::TypeName<Example::Internal::Array<int, 3>>
 	               == std::string_view{"Example::Internal::Array<int, 3>"});
 
 	STATIC_REQUIRE(SecUtility::TypeName<const Example::Internal::Array<int, 4>>
 	               == std::string_view{"const Example::Internal::Array<int, 4>"});
+#endif
 
 	REQUIRE(Matched(SecUtility::TypeName<Example::Internal::Array<int, 5>&>,
-	                std::regex{R"(^Example::Internal::Array<int, 5>\s*&$)"}));
+	                std::regex{R"(^(class )?Example::Internal::Array<int,\s?5>\s*&$)"}));
 
 	REQUIRE(Matched(SecUtility::TypeName<const Example::Internal::Array<int, 6>&>,
-	                std::regex{R"(^const Example::Internal::Array<int, 6>\s*&$)"}));
+	                std::regex{R"(^const (class )?Example::Internal::Array<int,\s?6>\s*&$)"}));
 
 	REQUIRE(Matched(SecUtility::TypeName<Example::Internal::Array<int, 7>&&>,
-	                std::regex{R"(^Example::Internal::Array<int, 7>\s*&&$)"}));
+	                std::regex{R"(^(class )?Example::Internal::Array<int,\s?7>\s*&&$)"}));
 
 	REQUIRE(Matched(SecUtility::TypeName<const Example::Internal::Array<int, 42>&&>,
-	                std::regex{R"(^const Example::Internal::Array<int, 42>\s*&&$)"}));
+	                std::regex{R"(^const (class )?Example::Internal::Array<int,\s?42>\s*&&$)"}));
 }
 
 #include <typeinfo>
 
 TEST_CASE("Demangle should work")
 {
+#if defined(_MSC_VER)
+	REQUIRE(SecUtility::Demangle(typeid(Example::Internal::Array<int, 3>).name())
+	        == std::string_view{"class Example::Internal::Array<int,3>"});
+#else
 	REQUIRE(SecUtility::Demangle(typeid(Example::Internal::Array<int, 3>).name())
 	        == std::string_view{"Example::Internal::Array<int, 3ul>"});
+#endif
 }
