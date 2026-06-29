c3b529532c8fecdcfcd023b2a64af4e742ae00f6
Andy Brown <55843047+andbrn743@users.noreply.github.com>
2026-06-29 22:14:20 +0800

    Define SignedIntegral and UnsignedIntegral

--- SecUtility/Raw/Int.hpp
+++ SecUtility/Raw/Int.hpp
@@ -37,3 +37,76 @@ namespace SecUtility
 #if defined(SECUTILITY_NO_TOP_LEVEL_INT_ALIAS) && SECUTILITY_NO_TOP_LEVEL_INT_ALIAS
 }
 #endif
+
+
+namespace SecUtility
+{
+	namespace Detail::Raw
+	{
+		template <std::size_t>
+		struct integer_type_with_size;
+
+		template <std::size_t S>
+		using integer_type_with_size_t = typename integer_type_with_size<S>::type;
+
+		template <std::size_t>
+		struct unsigned_integer_type_with_size;
+
+		template <std::size_t S>
+		using unsigned_integer_type_with_size_t = typename unsigned_integer_type_with_size<S>::type;
+
+		template <>
+		struct integer_type_with_size<8>
+		{
+			using type = std::int8_t;
+		};
+
+		template <>
+		struct integer_type_with_size<16>
+		{
+			using type = std::int16_t;
+		};
+
+		template <>
+		struct integer_type_with_size<32>
+		{
+			using type = std::int32_t;
+		};
+
+		template <>
+		struct integer_type_with_size<64>
+		{
+			using type = std::int64_t;
+		};
+
+		template <>
+		struct unsigned_integer_type_with_size<8>
+		{
+			using type = std::uint8_t;
+		};
+
+		template <>
+		struct unsigned_integer_type_with_size<16>
+		{
+			using type = std::uint16_t;
+		};
+
+		template <>
+		struct unsigned_integer_type_with_size<32>
+		{
+			using type = std::uint32_t;
+		};
+
+		template <>
+		struct unsigned_integer_type_with_size<64>
+		{
+			using type = std::uint64_t;
+		};
+	}
+
+	template <std::size_t SizeInBits>
+	using SignedIntegral = typename Detail::Raw::integer_type_with_size<SizeInBits>::type;
+
+	template <std::size_t SizeInBits>
+	using UnsignedIntegral = typename Detail::Raw::unsigned_integer_type_with_size<SizeInBits>::type;
+}
